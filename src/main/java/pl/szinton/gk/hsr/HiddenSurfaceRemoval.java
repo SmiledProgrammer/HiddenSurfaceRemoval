3528cfddb3cfd5fdd6d1280504ccdce3bb6b1580
szinton <szinton@gmail.com>
2022-05-19 11:42:14 +0200

    Hidden surface removal package

---
+++ src/main/java/pl/szinton/gk/hsr/HiddenSurfaceRemoval.java
@@ -0,0 +1,14 @@
+package pl.szinton.gk.hsr;
+
+import pl.szinton.gk.view.Camera3D;
+import pl.szinton.gk.view.Model3D;
+
+import java.awt.*;
+import java.util.List;
+
+public class HiddenSurfaceRemoval {
+
+    public static void render(Graphics2D g, Camera3D camera, List<Model3D> objects) {
+        // TODO
+    }
+}
